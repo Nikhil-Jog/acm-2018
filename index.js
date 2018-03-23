812a77683e2f0eb18679b1f5cacf4d269b8d4d7e
Deniz A. Sokullu <deniz.sokullu@ibm.com>
2018-03-23 18:56:35 -0400

    added nodejs fixes

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ app.use(express.static(path.join(__dirname,'./static')));
 
 
 app.get('/',(req, res) => {
-  res.sendFile('./static/index.html')
+  res.sendFile(__dirname + '/static/index.html')
 });
 
 app.get('/aboutUs',(req, res) => {
-  res.sendFile('./static/about-us.html')
+  res.sendFile(__dirname + '/static/about-us.html')
 });
 
 app.get('/services',(req, res) => {
-  res.sendFile('./static/services.html')
+  res.sendFile(__dirname + '/static/services.html')
 });
 
 app.get('/events',(req, res) => {
-  res.sendFile('./static/portfolio.html')
+  res.sendFile(__dirname + '/static/portfolio.html')
 });
 
 app.get('/contact',(req, res) => {
-  res.sendFile('./static/contact-us.html')
+  res.sendFile(__dirname + '/static/contact-us.html')
 });
 
 const PORT = process.env.PORT || 8000;
